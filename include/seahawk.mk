e4aee08e2d3f130724e33bf8389761f0d0944270
Alexander Gabert <alexander.gabert@gmail.com>
2015-09-01 20:55:36 +0200

    add all roles as args to the files

--- include/seahawk.mk
+++ include/seahawk.mk
@@ -1,15 +1,7 @@
 
 #
-# the pssh macro to run the scripts on the machines
+# temp directory to store server lists as text files
 #
-PSSH_OPTS = -O StrictHostKeyChecking=no
-
-PSSH1 = pssh $(PSSH_OPTS) -I --hosts="tmp/$(@).txt" -- "tee /tmp/$(@).sh | chmod 0755 /tmp/$(@).sh" <"usr/bin/$(@).sh"
-
-PSSH2 = pssh $(PSSH_OPTS) -i -v --hosts="tmp/$(@).txt" -- "/tmp/$(@).sh"
-
-PSSH = echo "running pssh task $(@)" && $(PSSH1) && $(PSSH2)
-
 TMPDIR = tmp
 
 #
@@ -27,24 +19,46 @@ ifeq "$(CONFIG_CONTROLLER_HOST)" ""
 	CONFIG_CONTROLLER_HOST = $(shell bin/answer.sh "$(ANSWERFILE)" "CONFIG_CONTROLLER_HOST")
 endif
 
-SERVERFILES = tmp/controller.txt tmp/compute.txt tmp/gateways.txt tmp/nsdb.txt
+SERVERFILES = $(TMPDIR)/controller.txt $(TMPDIR)/compute.txt $(TMPDIR)/gateways.txt $(TMPDIR)/nsdb.txt
 
 .PHONY: $(SERVERFILES)
 
 preflight: $(SERVERFILES)
+	test -f "$(ANSWERFILE)" || exit 1
 
 $(TMPDIR):
 	@mkdir -pv $(TMPDIR)
 
-tmp/controller.txt: $(TMPDIR)
+$(TMPDIR)/controller.txt: $(TMPDIR)
 	echo "$(CONFIG_CONTROLLER_HOST)" > $(@)
 
-tmp/compute.txt: $(TMPDIR)
+$(TMPDIR)/compute.txt: $(TMPDIR)
 	echo "$(CONFIG_COMPUTE_HOSTS)" > $(@)
 
-tmp/gateways.txt: $(TMPDIR)
+$(TMPDIR)/gateways.txt: $(TMPDIR)
 	cat conf/gateways.txt > $(@)
 
-tmp/nsdb.txt: $(TMPDIR)
+$(TMPDIR)/nsdb.txt: $(TMPDIR)
 	(echo "$(CONFIG_CONTROLLER_HOST)"; echo "$(CONFIG_COMPUTE_HOSTS)") | sort | uniq > $(@)
+
+#
+# the pssh macro to run the scripts on the machines
+#
+PSSH_OPTS = -O StrictHostKeyChecking=no
+
+ROLES = "$(shell cat $(TMPDIR)/controller.txt)" "$(shell cat $(TMPDIR)/compute.txt)" "$(shell cat $(TMPDIR)/gateways.txt)" "$(shell cat $(TMPDIR)/nsdb.txt)"
+
+#
+# prepare script (will install epel, MEM repo and puppet module from forge)
+#
+PSSH1 = pssh $(PSSH_OPTS) -I --hosts="$(TMPDIR)/$(@).txt" -- "tee /tmp/prepare.sh | chmod 0755 /tmp/prepare.sh" <"usr/bin/prepare.sh"
+PSSH2 = pssh $(PSSH_OPTS) -i -v --hosts="$(TMPDIR)/$(@).txt" -- "/tmp/prepare.sh" $(ROLES)
+
+#
+# run the script of the role for this box
+#
+PSSH3 = pssh $(PSSH_OPTS) -I --hosts="$(TMPDIR)/$(@).txt" -- "tee /tmp/$(@).sh | chmod 0755 /tmp/$(@).sh" <"usr/bin/$(@).sh"
+PSSH4 = pssh $(PSSH_OPTS) -i -v --hosts="$(TMPDIR)/$(@).txt" -- "/tmp/$(@).sh" $(ROLES)
+
+PSSH = echo "running pssh task $(@)" && $(PSSH1) && $(PSSH2) && $(PSSH3) && $(PSSH4)
 
